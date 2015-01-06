b4aa7d39bfe46cb25eccc8c5b8e01f61729afb8c
Logan Serman <logan.serman@metova.com>
2015-01-05 19:04:44 -0600

    Defaulting aws_access_key_id and aws_secret_access_key to ENV

--- lib/elbas/aws.rb
+++ lib/elbas/aws.rb
@@ -50,9 +50,9 @@ module Elbas
     private
 
       def credentials
-        _credentials = { 
-          access_key_id: fetch(:aws_access_key_id), 
-          secret_access_key: fetch(:aws_secret_access_key), 
+        _credentials = {
+          access_key_id: fetch(:aws_access_key_id, ENV['AWS_ACCESS_KEY_ID']),
+          secret_access_key: fetch(:aws_secret_access_key, ENV['AWS_SECRET_ACCESS_KEY']),
         }
         _credentials.merge(region: fetch(:aws_region)) if fetch(:aws_region)
       end

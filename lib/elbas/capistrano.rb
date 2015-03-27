e871b63beb8a4afe49ad3edfb78d7e6193fbd09d
Logan Serman <logan.serman@metova.com>
2015-03-27 13:34:33 -0500

    Add message when no instances were found

--- lib/elbas/capistrano.rb
+++ lib/elbas/capistrano.rb
@@ -7,14 +7,20 @@ def autoscale(groupname, *args)
   include Capistrano::DSL
   include Elbas::AWS::AutoScaling
 
-  autoscale_group = autoscaling.groups[groupname]
+  autoscale_group   = autoscaling.groups[groupname]
+  running_instances = autoscale_group.ec2_instances.filter('instance-state-name', 'running')
+
   set :aws_autoscale_group, groupname
 
-  autoscale_group.ec2_instances.filter('instance-state-name', 'running').each do |instance|
+  running_instances.each do |instance|
     hostname = instance.dns_name || instance.private_ip_address
     p "ELBAS: Adding server: #{hostname}"
     server(hostname, *args)
   end
 
-  after('deploy', 'elbas:scale')
+  if running_instances.size > 0
+    after('deploy', 'elbas:scale')
+  else
+    p "ELBAS: AMI could not be created because no running instances were found. Is your autoscale group name correct?"
+  end
 end

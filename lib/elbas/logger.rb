a50693b7fa58297d5be303e1f9a9b404e0ef157f
Logan Serman <loganserman@gmail.com>
2019-02-18 15:41:27 -0500

    Use Capistrano output helpers in our logger to make it prettier

--- lib/elbas/logger.rb
+++ lib/elbas/logger.rb
@@ -1,7 +1,15 @@
+require 'capistrano/doctor/output_helpers'
+
 module Elbas
   module Logger
+    include Capistrano::Doctor::OutputHelpers
+
     def info(message)
-      p "ELBAS: #{message}"
+      $stdout.puts [prefix, message, "\n"].join
+    end
+
+    def prefix
+      @prefix ||= color.colorize("\n[ELBAS] ", :cyan)
     end
   end
 end

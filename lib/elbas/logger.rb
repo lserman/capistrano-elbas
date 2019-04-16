6da462e0162711b6ccaebc95ea134c76a917a15f
Charles Smith <charles@beachyapp.com>
2019-04-16 15:32:35 -0500

    Logger#error is now a method. Added test for no servers case

--- lib/elbas/logger.rb
+++ lib/elbas/logger.rb
@@ -4,17 +4,35 @@ module Elbas
   module Logger
     include Capistrano::Doctor::OutputHelpers
 
+    PREFIX_TEXT = '[ELBAS] '
+
     def info(message)
       $stdout.puts [prefix, message, "\n"].join
     end
 
+    def error(message)
+      $stderr.puts [error_prefix, message, "\n"].join
+    end
+
     def cyan(text)
-      color.colorize text, :cyan
+      color_text text, :cyan
+    end
+
+    def red(text)
+      color_text text, :red
+    end
+
+    def color_text(text, coloring)
+      color.colorize text, coloring
     end
 
     private
       def prefix
-        @prefix ||= cyan('[ELBAS] ')
+        @prefix ||= cyan(PREFIX_TEXT)
+      end
+
+      def error_prefix
+        @error_prefix ||= red(PREFIX_TEXT)
       end
   end
 end
